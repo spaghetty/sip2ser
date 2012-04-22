0cdf5597c9322e402bfb9eb2b17730603dfaa519
luciano <luciano@testhost.rpm.net>
2012-04-22 02:48:13 +0200

    net-scp rpm generation

---
+++ libs/rubygem-net-scp/.sipxecs.mk
@@ -0,0 +1,6 @@
+rubygem-net-scp_SRPM = rubygem-net-scp-1.0.4-1$(RPM_DIST).src.rpm
+rubygem-net-scp_SPEC = $(SRC)/$(PROJ)/rubygem-net-scp.spec
+rubygem-net-scp_SOURCES = $(SRC)/$(PROJ)/net-scp-1.0.4.gem
+
+# targets not defined, nothing to do
+rubygem-net-scp.autoreconf rubygem-net-scp.configure rubygem-net-scp.dist:;
