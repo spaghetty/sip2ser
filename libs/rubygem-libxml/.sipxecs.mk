dde168c3befb8a10a51b2ee9cd0ba3043eaf4287
sip2ser <sip2ser@opsio.sip2serlab.net>
2012-12-03 11:01:46 +0100

    massive clean

--- libs/rubygem-libxml/.sipxecs.mk
+++
@@ -1,6 +0,0 @@
-rubygem-libxml_SRPM = rubygem-libxml-ruby-2.3.2-1$(RPM_DIST).src.rpm
-rubygem-libxml_SPEC = $(SRC)/$(PROJ)/rubygem-libxml.spec
-rubygem-libxml_SOURCES = $(SRC)/$(PROJ)/libxml-ruby-2.3.2.gem
-
-# targets not defined, nothing to do
-rubygem-libxml.autoreconf rubygem-libxml.configure rubygem-libxml.dist:;
--- libs/rubygem-treetop/.sipxecs.mk
+++
@@ -1,6 +0,0 @@
-rubygem-treetop_SRPM = rubygem-treetop-1.4.9-1$(RPM_DIST).src.rpm
-rubygem-treetop_SPEC = $(SRC)/$(PROJ)/rubygem-treetop.spec
-rubygem-treetop_SOURCES = $(SRC)/$(PROJ)/treetop-1.4.9.gem
-
-# targets not defined, nothing to do
-rubygem-treetop.autoreconf rubygem-treetop.configure rubygem-treetop.dist:;
