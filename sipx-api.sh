02bf2dd6ffe1ec61372260f580d88e04ab8018ed
Luciano Berardi <berardi.luciano@gmail.com>
2013-01-08 12:00:11 +0100

    going on with api description and samples

--- sipx-api.sh
+++ sipx-api.sh
@@ -2,8 +2,8 @@
 
 #USER
 api=$1
-user='208'
-password='208208123'
+user='200'
+password='200200123'
 host=192.168.69.51
 
 
@@ -15,23 +15,50 @@ echo "[USER2] /my/phonebook"
 echo "[USER3] /my/pagedphonebook?start={start-row}&amp;end={end-row}"
 echo "[USER4] /my/phonebook/entry/{entryId}"
 echo "[USER5] /my/phonebook/showContactsOnPhone/{value}"
-echo "[USER6] /my/phonebook/googleImport"
-echo "[USER7] /my/call/{to}"
-echo "[USER8] /my/voicemail/pin/{pin}"
-echo "[USER9] /my/voicemail/operator/{operator}"
-echo "[USER10] /my/voicemail/operator/"
-echo "[USER11] /my/forward"
-echo "[USER12] /my/feed/voicemail/{folder}"
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/contact-information
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/time
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/mailbox/{user}/preferences/activegreeting/{greeting}
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/conferences
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/conference/{confName}/{command}
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/activecdrs
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/logindetails
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/redirect
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/conferencedetails/{confName}
+echo "[USER6] /my/call/{to}"
+echo "[USER7] /my/voicemail/pin/{pin}"
+echo "[USER8] /my/voicemail/operator/{operator}"
+echo "[USER9] /my/voicemail/operator/"
+echo "[USER10] /my/forward"
+echo "[USER11] /my/feed/voicemail/{folder}"
+echo "[USER12] /my/contact-information"
+echo "[USER13] /my/time"
+echo "[USER14] /my/mailbox/{user}/preferences/activegreeting/{greeting}"
+echo "[USER15] /my/conferences"
+echo "[USER16] /my/conference/{confName}/{command}"
+echo "[USER17] /my/activecdrs"
+echo "[USER18] /my/logindetails"
+echo "[USER19] /my/redirect"
+echo "[USER20] /my/conferencedetails/{confName}"
+echo "[USER21] /my/phonebook/googleImport"
+echo "[CDR1] /cdr/{user}?limit=maxrecs&fromdate=yyyymmdd"
+echo "[CALLCTRL1] /callcontroller/callingParty/calledParty GET method"
+echo "[CALLCTRL2] /callcontroller/callingParty/calledParty POST method"
+echo "[ATT1] /auto-attendant"
+echo "[ATT2] /auto-attendant/{attendant}/special PUT method"
+echo "[ATT3] /auto-attendant/{attendant}/special DELETE method"
+echo "[ATT4] /auto-attendant/specialmode GET method"
+echo "[ATT5] /auto-attendant/specialmode PUT method"
+echo "[ATT6] /auto-attendant/specialmode DELETE method"
 
+
+if test $api = 'CDR1'; then
+    echo "Retrieve dei CDR per l'utente nel persiodo specificati in GET"
+    echo curl --digest -k -X GET "http://$user:$password@$host:6666/cdr/$user?limit=10&fromdate=20121101"
+    curl --digest -k -X GET "http://$user:$password@$host:6666/cdr/$user?fromdate=20121101&limit=2"
+fi
+
+if test $api = 'CALLCTRL1'; then
+    echo ""
+    echo curl --digest -k -X GET "http://$user:$password@$host:6666/callcontroller/$user/201"
+    curl -k --digest -X GET "http://$user:$password@$host:6666/callcontroller/$user/201"
+fi
+
+if test $api = 'CALLCTRL2'; then
+    echo ""
+    echo curl --digest -k -X POST -u $user:$password "http://$host:6666/callcontroller/$user/203?agent=$user&sipMethod=INVITE"
+    curl --digest -k -X POST -u $user:$password "http://$host:6666/callcontroller/$users/203?agent=$user&sipMethod=INVITE"
+fi
 
 if test $api = 'USER1'; then
     echo "Rtrieve delle info relative a uno specifico contatto (207 )nella rubrica dell'utente 200"
@@ -61,63 +88,88 @@ if test $api = 'USER5'; then
     echo "To be reviewed..."
 fi
 if test $api = 'USER6'; then
-    echo "Import contacts from google"
-    echo curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/phonebook/googleImport"
-    #curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/phonebook/googleImport"
-    echo "To be reviewed..."
-fi
-if test $api = 'USER7'; then
     echo "Effettua una chiamata dall'utente 200 al 207"
     echo curl --digest -k -X PUT "http://$user:$password@$host/sipxconfig/rest/my/call/207"
     curl --digest -k -X PUT "http://$user:$password@$host/sipxconfig/rest/my/call/207"
 fi
-if test $api = 'USER8'; then
+if test $api = 'USER7'; then
     echo "Effettua il cambio PIN per l'accesso alla Voicemail"
     echo curl --digest -k -X PUT "https://$user:$password@$host/sipxconfig/rest/my/voicemail/pin/500"
     curl --digest -k -X PUT "http://$user:$password@$host/sipxconfig/rest/my/voicemail/pin/500"
 fi
-if test $api = 'USER9'; then
+if test $api = 'USER8'; then
     echo "Effetua la modifica dell'operatore (es.: una estensione) associato alla casella vocale"
     echo curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/voicemail/operator/{operator}"
-    curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/voicemail/operator/207"
+    curl --digest -k -X PUT "http://$user:$password@$host/sipxconfig/rest/my/voicemail/operator/207"
+    echo "To be reviewed...mailboxmanager Error"
+fi
+if test $api = 'USER9'; then
+    echo "Recupera il varore dell'operatore configurato per il personal autoattendant"
+    echo curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/voicemail/operator"
+    #curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/voicemail/operator"
+    echo "To be reviewed...404 Not found"
 fi
 if test $api = 'USER10'; then
-    curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/phonebook"
+    echo "Recupera i dettagli relativi al Call forwarding dell'utente"
+    echo curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/forward"
+    curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/forward"
 fi
 if test $api = 'USER11'; then
-    curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/search/phonebook?query=207"
+    echo "Non e' chiaro a cosa serva"
+    echo curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/feed/voicemail/{folder}"
+    #curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/feed/voicemail/208"
 fi
 if test $api = 'USER12'; then
-    curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/phonebook"
-fi
-if test $api = 'USER13'; then
-    curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/search/phonebook?query=207"
-fi
-if test $api = 'USER14'; then
-    curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/phonebook"
-fi
-if test $api = 'USER115'; then
-    curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/search/phonebook?query=207"
-fi
-if test $api = 'USER16'; then
-    curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/phonebook"
-fi
-if test $api = 'USER17'; then
-    curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/search/phonebook?query=207"
-fi
-if test $api = 'USER18'; then
-    curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/phonebook"
-fi
-if test $api = 'USER19'; then
-    curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/search/phonebook?query=207"
-fi
-if test $api = 'USER20'; then
-    curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/phonebook"
-fi
-if test $api = 'USER21'; then
-    curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/search/phonebook?query=207"
+    echo "Recupera le informazioin dell'utente "
+    echo curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/contact-information"
+    curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/contact-information"
 fi
 
+if test $api = 'USER13'; then
+    echo "Recupera data e ora del server host"
+    echo curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/time"
+    curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/time"
+fi
+if test $api = 'USER14'; then
+    echo curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/mailbox/$user/preferences/activegreeting/2"
+    #curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/mailbox/$user/preferences/activegreeting/standard"
+fi
+if test $api = 'USER15'; then
+    echo "elenca tutte le conferenze associate all'utente"
+    echo curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/conferences"
+    curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/conferences"
+fi
+if test $api = 'USER16'; then
+    echo "esegue un comando per la conferenza dell'utente"
+    echo curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/conference/test/"
+    curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/conference/test/lock"
+fi
+if test $api = 'USER17'; then
+    echo "mostra i dettagli sulle chiamate attive dell'utente"
+    echo curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/activecdrs"
+    curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/activecdrs"
+fi
+if test $api = 'USER18'; then
+    echo "Mostra i dettagli delle login dell'utente: UserName, imId .."
+    echo curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/logindetails"
+    curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/logindetails"
+fi
+if test $api = 'USER19'; then
+    echo curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/redirect"
+    #curl --digest -k -X GET "http://$user:$password@$host/sipxconfig/rest/my/redirect"
+fi
+if test $api = 'USER20'; then
+    echo "restituisce tutti i parametri della conferenza attiva"
+    echo curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/conferencedetails/test"
+    curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/conferencedetails/test"
+fi
+
+if test $api = 'USER21'; then
+    echo "Import contacts from google"
+    echo curl --digest -k -X POST "https://$user:$password@$host/sipxconfig/rest/my/phonebook/googleImport"
+    #curl --digest -k -X POST "https://$user:$password@$host/sipxconfig/rest/my/phonebook/googleImport?account='notifiche@sip2ser.it'&password='prova123'"
+    echo "To be reviewed..."
+fi
 
 
 # OpenACD
@@ -145,26 +197,106 @@ fi
 
 
 # AutoAttendant
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/auto-attendant
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/auto-attendant/{attendant}/special
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/auto-attendant/specialmode
+if test $api = 'ATT1'; then
+    echo "elenca tutti gli autorisponditori settati in Opsip"
+    echo curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/auto-attendant"
+    curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/auto-attendant"
+fi
+
+if test $api = 'ATT2'; then
+    echo "Imposta l'attendant specificato come special attendant da usare" 
+    echo curl --digest -k -X PUT -u superadmin:superadmin "http://$host/sipxconfig/rest/auto-attendant/operator/special"
+    curl --digest -k -X PUT -u superadmin:superadmin "http://$host/sipxconfig/rest/auto-attendant/operator/special"
+fi
+
+if test $api = 'ATT3'; then
+    echo "Ripulisce il valore del parametro dell'attendant specificato come special attendant da usare" 
+    echo curl --digest -k -X DELETE -u superadmin:superadmin "http://$host/sipxconfig/rest/auto-attendant/operator/special"
+    curl --digest -k -X DELETE -u superadmin:superadmin "http://$host/sipxconfig/rest/auto-attendant/operator/special"
+fi
+
+if test $api = 'ATT4'; then
+    echo "Restituisce il valore del flag specialmode per autoattendant"
+    echo curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/my/phonebook/googleImport"
+    curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/auto-attendant/specialmode"
+fi
+
+if test $api = 'ATT5'; then
+    echo "Imposta il valore del flag specialmode per autoattendant a true"
+    echo curl --digest -k -X PUT "http://$host/sipxconfig/rest/auto-attendant/specialmode"
+    curl --digest -k -X PUT -u superadmin:superadmin "http://$host/sipxconfig/rest/auto-attendant/specialmode"
+fi
+
+if test $api = 'ATT6'; then
+    echo "Imposta il valore del flag specialmode per autoattendant a false"
+    echo curl --digest -k -X DELETE "http://$host/sipxconfig/rest/auto-attendant/specialmode"
+    curl --digest -k -X DELETE -u superadmin:superadmin "http://$host/sipxconfig/rest/auto-attendant/specialmode"
+fi
 
 
 # Phone and Phonebook
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/phone
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/phonebook/{name}
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/phonebook
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/phone/{serialNumber}/profile/{name}
+if test $api = 'PHONE1'; then
+    echo "Consente di creare un telefono utilizzando un file xml per definire SerialNumber model e description"
+    echo     curl --digest -k -X POST -u superadmin:superadmin -d @phone.xml "http://$host/sipxconfig/rest/phone"
+    curl --digest -k -X POST -u superadmin:superadmin -d @phone.xml "http://$host/sipxconfig/rest/phone"
+fi
+
+if test $api = 'PHONE2'; then
+    echo "restituisce il profilo di un dato telefono presente in Opsip sulla vase del suo serial number"
+    echo curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/phone/000413292b5e/profile/000413292B5E.xml" 
+    curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/phone/000413292b5e/profile/000413292B5E.xml" 
+fi
+
+if test $api = 'PHONEBOOK1'; then
+    echo "Restutiuisce i contatti presenti in una specifica rubrica telefonica"
+    echo curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/phonebook/PhoneBookProva"
+    curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/phonebook/PhoneBookProva"
+fi
+
+if test $api = 'PHONEBOOK2'; then
+    echo "Restituisce l'elenco di tutte le rubriche telefoniche in Opsip presenti nella sezione phonebooks"
+    echo curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/phonebook"
+    curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/phonebook"
+fi
+
 
 # Permissions
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/permission
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/permission/{name}
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/user-group-permission
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/user-group-permission/{id}
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/user-permission
-# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/user-permission/{id}
+if test $api = 'PERM1'; then
+    echo "Restituisce l'elenco di tutte le permission definite per i dialing plan"
+    echo  curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/permission"
+    curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/permission"
+fi
 
-# Others
+if test $api = 'PERM2'; then
+    echo "Restituisce i dettagli di una specifica permission"
+    echo curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/permission/900Dialing"
+    curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/permission/900Dialing"
+fi
+
+if test $api = 'PERM3'; then
+    echo "mostra lo stato delle permission per tutti gli user groups"
+    echo curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/phonebook"
+    curl --digest -k -X PUT -d @perm.xml -u superadmin:superadmin "http://$host/sipxconfig/rest/user-group-permission"
+fi
+if test $api = 'PERM4'; then
+    echo "mostra lo stato delle permission per uno specifico user-group" 
+    echo     curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/user-group-permission/4"
+    curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/user-group-permission/4"
+fi
+
+if test $api = 'PERM5'; then
+    echo "Restituisce l'elenco di tutte le rubriche telefoniche in Opsip presenti nella sezione phonebooks"
+    echo curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/phonebook"
+# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/user-permission
+fi
+
+if test $api = 'PHONEBOOK2'; then
+    echo "Restituisce l'elenco di tutte le rubriche telefoniche in Opsip presenti nella sezione phonebooks"
+    echo curl --digest -k -X GET -u superadmin:superadmin "http://$host/sipxconfig/rest/phonebook"
+# % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/user-permission/{id}
+fi
+
+# Other
 # % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/avatar/{user}
 # % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/branch
 # % >> curl --digest -k -X GET "https://$user:$password@$host/sipxconfig/rest/branch/{id}
